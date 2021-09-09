7904ea9af0e2f3ee7c98670000793e259a275e25
wuyongkang <2216855594@qq.com>
2021-09-09 15:36:23 +0800

    Revert "Revert "312412""
    This reverts commit f1f44372d31c7c05d4893614106ec8c984486dcb.

--- wisper-service/src/main/java/com/wyk/wisper/controller/UserController.java
+++ wisper-service/src/main/java/com/wyk/wisper/controller/UserController.java
@@ -93,8 +93,6 @@ public class UserController {
     }
 
     public static void main(String[] args) {
-        System.out.println(new A().hashCode());
-        System.out.println(new A());
-        System.out.println(new A().hashCode());
+
     }
 }
